226c610dc4179d389eadf1af7a7a28cca316afed
Mohd Wasi Khan <mwkhanb23@gmail.com>
2026-08-25 16:48:20 +0530

    Time: 82 ms (6.23%), Space: 151.3 MB (37.48%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,62 @@
 class MinStack {
 public:
-    stack<pair<int,int>>st;
+    // stack<pair<int,int>>st;
+    long long mini=INT_MAX;
+    stack<long long>st;
     MinStack() {
         
     }
     
     void push(int value) {
+
         if(st.empty()){
-            st.push({value,value});
+            mini=value;
+            st.push(value);
         }
         else{
-            st.push({value,min(value,st.top().second)});
+            if(mini>value){
+                long long newval=2LL*value-mini;
+                mini=value;
+                st.push(newval);
+            }else{
+                st.push(value);
+            }
         }
+
+        // if(st.empty()){
+        //     st.push({value,value});
+        // }
+        // else{
+        //     st.push({value,min(value,st.top().second)});
+        // }
     }
     
     void pop() {
+        // st.pop();
+        if(st.empty()){
+            return;
+        }
+        if(st.top()<mini){
+            long long preval=2LL*mini-(st.top());
+            mini=preval;
+        }
         st.pop();
     }
     
     int top() {
-        return st.top().first;
+        if(st.empty()){
+            return -1;
+        }
+        else if(st.top()<mini){
+            return mini;
+        }
+        else{
+            return st.top();
+        }
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
